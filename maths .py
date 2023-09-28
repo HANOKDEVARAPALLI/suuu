8486e073f7e11060c04e7bb3ee96ec67167437ab
HANOKDEVARAPALLI <hanokdevarapalli@gmail.com>
2023-09-28 10:00:10 +0530

    second commit

---
+++ maths .py
@@ -0,0 +1,9 @@
+num1=int(input("enter firts value:"))
+num2=int(intput("enter second value:"))
+num3=int(input("enter third value :"))
+if num1 < num2 and num1 < num3:
+    print(f"num1 is smallest")
+elif num2 < num1 and num2 < num3:
+    print(f"num2 is smallest")
+else:
+    print(f"num3 is smallest")23
